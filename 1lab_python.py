8583bd09f79c1ce19614b4c71ec5238d87b1eb0a
sfx18 <49839343+sfx18@users.noreply.github.com>
2019-06-19 17:17:27 +0300

    Update 1lab_python.py

--- 1lab_python.py
+++ 1lab_python.py
@@ -6,7 +6,7 @@ import scipy.optimize as so
 import matplotlib.pyplot as plt
 
 
-print ('\nN1 Îáúÿâèòü ñëåäóþùèå ìàòðèöû è âåêòîðû\n')
+
 
 A = np.array([[1, 2, 3], [-2,3,0],[5,1,4]])
 print (str(A) + ' = A\n')
@@ -17,14 +17,14 @@ print (str(u) + ' = u\n')
 v = np.array([[3], [2], [10]])
 print (str(v) + ' = v\n')
 
-print ('\nN2 Îáúÿâèòü ñëó÷àéíóþ ìàòðèöó C (100x100) è ñëó÷àéíûé âåêòîð w (100?1)\n')
+
 
 C = np.random.random((100, 100))
 print (str(C) + ' = C\n')
 w = np.random.random((100, 1))
 print (str(w) + ' = w\n')
 
-print ('\nN3 Âû÷èñëèòü âûðàæåíèÿ\n')
+
 
 print (str(A + B) + ' = A + B\n')
 print (str(A * B) + ' = AB\n')
@@ -35,30 +35,30 @@ print (str(u * v) + ' = uv\n')
 print (str(C * w) + ' = Cw\n')
 print (str(w.T * C) + ' = (w^T)C\n')
 
-print ('\nN4 Ïîñòðîèòü ìàòðèöó ðàçìåðà 20?20, ýëåìåíòû êîòîðîé ðàâíû ïðîèçâåäåíèÿì èíäåêñîâ\n')
+
 
 D = np.fromfunction(lambda i,j : i*j, (20, 20))
 print(str(D).replace('.','').replace('  ',' '))
 
-print ('\nN5 Ñîõðàíèòü ìàòðèöû è âåêòîðû â îäèí îáùèé ôàéë\n')
+
 
 #sio.savemat ('data.mat', {'A': A})
 #A1 = sio.loadmat('data.mat')['A']
 #print (A1)
 
-print ('\nN6 Íàéòè ñóììó ïîëîæèòåëüíûõ ýëåìåíòîâ ìàòðèö $A$ è $B$\n')
+
 
 print (str(A[A>0].sum()) + ' = Summa A[A>0]\n')
 print (str(B[B>0].sum()) + ' = Summa B[B>0]\n')
 print (str(A[A>0].sum()+B[B>0].sum()) + ' = Summa A+B[A,B>0]\n')
 
-print('\nN7 Çàïèñàòü ýëåìåíòû ìàòðèöû A â îäíó ñòðîêó è âûáðàòü èç íèõ òîëüêî ñòîÿùèå íà ÷¸òíûõ ïîçèöèÿõ\n')
+
 
 AA = np.reshape(A, (1, 9))
 print(AA)
 print(str(AA[:,1::2]) + ' = chetnie pozicii\n')
 
-print('\nN8 Íàéòè îáðàòíûå è ïñåâäîîáðàòíûå ìàòðèöû äëÿ A, B è Ñ\n')
+
 
 print (str(li.inv(A)) + ' = obratnaj A\n')
 print (str(li.pinv(B)) + ' = psevdoobratnaj A\n')
@@ -67,7 +67,7 @@ print (str(li.inv(A).dot(A)) + ' = edenichnaj A\n')
 print (str(li.pinv(B).dot(B)) + ' = edenichnaj B\n')
 print (str(li.inv(C).dot(C)) + ' = edenichnaj C\n')
 
-print('\nN9 Ðåøèòü ñèñòåìó óðàâíåíèé\n')
+
 
 H = np.array([[32, 7, -6], [-5, -20, 3], [0, 1, -3]])
 J = np.array([12, 3, 7])
@@ -79,12 +79,12 @@ print(lj[0, :].sum())
 print(lj[1, :].sum())
 print(lj[2, :].sum())
 
-print('\nN10 Íàéòè ñîáñòâåííûå çíà÷åíèÿ è ñîáñòâåííûå âåêòîðû ìàòðèö $A$ è $B$\n')
+
 
 print (li.eig(A))
 print (li.eig(B))
 
-print('\nN11 Íàéòè ìèíèìóìû ôóíêöèé\n')
+
 
 def f1(x):
     return 5*(x-2)**4 - 1/(x**2+8)
@@ -100,7 +100,7 @@ def df2(x):
 x_min = so.minimize(f2, [0.0, 0.0], method='BFGS', jac=df2)
 print ('f2(x1,x2) = 4(x1-3*x2)^2 + 7*x1^4\n' + str(x_min))
 
-print('\nN12 Ïîñòðîèòü ãðàôèêè ôóíêöèé â îäíîé ñèñòåìå êîîðäèíàò\n')
+
 
 def g1(x):
     return x**5 - 2*x**4 + 3*x - 7
